2c2ea1101fa8e071644ec0c0c91f1d941c22e99e
Roman Gomolko <rgomolko@gmail.com>
2015-11-21 23:27:33 +0200

    1.1: Saving data into DynamoDB

--- src/api/index.js
+++ src/api/index.js
@@ -3,6 +3,10 @@ var bodyParser = require('body-parser');
 var shortHash = require('short-hash');
 var LocalStorage = LocalStorage;
 var storage = new (require('node-localstorage').LocalStorage)('./storage');
+var AWS = require("aws-sdk");
+AWS.config.update({region: 'eu-west-1'});
+
+var db = new AWS.DynamoDB.DocumentClient();
 
 var app = express();
 app.use(express.static(__dirname + '/public'));
@@ -18,17 +22,37 @@ app.post('/shorten', function (req, res) {
         return res.status(400).json({message: 'no URL'}).end();
     } else {
         var hash = shortHash(url);
-        storage.setItem(hash, url);
-        res.json({hash: hash, shortUrl: process.env.HOST + hash, targetUrl: url});
+        db.put({
+            TableName: "short_links",
+            Item: {"hash": hash, targetUrl: url}
+        }, function () {
+            storage.setItem(hash, url);
+            res.json({hash: hash, shortUrl: process.env.HOST + hash, targetUrl: url});
+        })
     }
 });
 
-app.get('/:key', function (req, res) {
-    var url = storage.getItem(req.params.key);
+function go(res, url) {
     if (url) {
         res.redirect(302, url);
     } else {
         res.status(404).send('Sorry, we cannot find that!');
+    }
+}
+
+app.get('/:key', function (req, res) {
+    var hash = req.params.key;
+    var url = storage.getItem(hash);
+    if (!url) {
+        db.get(
+            {TableName: "short_links", Key: {"hash": hash}},
+            function (err, record) {
+                url = record.targetUrl;
+                url && storage.setItem(hash, url);
+                go(res, url);
+            });
+    } else {
+        go(res, url);
     }
 });
 
